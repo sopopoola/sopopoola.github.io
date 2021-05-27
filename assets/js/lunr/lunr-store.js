1ef67aef3b09a7cdbe3ce55c74f6fef8e7642590
popoola <sopopoola@crimson.ua.edu>
2021-05-27 15:30:45 -0500

    initial site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Bananas",
+        "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": [],
+        "tags": [],
+        "url": "/bananas/",
+        "teaser": null
+      },{
+        "title": "Apples",
+        "excerpt":"An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands...","categories": [],
+        "tags": [],
+        "url": "/apples/",
+        "teaser": null
+      }]
