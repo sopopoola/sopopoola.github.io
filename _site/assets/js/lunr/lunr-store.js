6154105eb8ed8b153e2963402a17df30f01fe37f
Saheed Popoola <25807285+sopopoola@users.noreply.github.com>
2025-07-08 16:06:17 +0100

    Updated Web page
    Put the webpage up to date

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Cs_it",
+        "excerpt":"Computer Science Vs Information Techology   coming soon  ","categories": [],
+        "tags": [],
+        "url": "/CS_IT/",
+        "teaser": null
+      },{
+        "title": "Educational_software_engineering",
+        "excerpt":"Educational Software Engineering   Coming Soon  ","categories": [],
+        "tags": [],
+        "url": "/Educational_Software_Engineering/",
+        "teaser": null
+      }]
